ef6924a40dd018a3aaf729305049983432526d89
Grzesiek <fundaysunday6@gmail.com>
2017-10-11 12:55:30 +0200

    improved stats script

--- testmodes/g-n-ch1-ch6/results/calcMeanAndStdDev.py
+++ testmodes/g-n-ch1-ch6/results/calcMeanAndStdDev.py
@@ -16,14 +16,15 @@ def main(argv):
                 results.append(float(line[1]))
         meanVal = statistics.mean(results)
         stdDevVal = statistics.stdev(results)
-        meanString = "Mean value for " + file + " is: " + str(meanVal)
-        stdDevString = "Standard Deviation value for " + \
-            file + " is: " + str(stdDevVal)
-        print meanString
-        print stdDevString
+        pm = file.split("_")[1]
+        sched = file.split("_")[2].split(".")[0]
+        meanString = "Mean value for is: " + str(meanVal)
+        stdDevString = "Standard Deviation value for is: " + str(stdDevVal)
         with open("statsResults.txt", "a+") as fw:
+            fw.write("Path Manager: " + pm + " and Scheduler: " + sched + "\n")
             fw.write(meanString + "\n")
             fw.write(stdDevString + "\n")
+            fw.write("-" * 50 + "\n")
 
 
 if __name__ == "__main__":
