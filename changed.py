4069d3a0e360d41634502fcd09b884d3831b73bf
floppydrive6 <grzeg.przybylo@gmail.com>
2017-10-23 23:47:06 +0200

    Update changed.py

--- changed.py
+++ changed.py
@@ -11,7 +11,7 @@ from mininet.cli import CLI
 from mininet.net import Mininet
 from mininet.link import TCLink
 from mininet.log import setLogLevel
-from mininet.node import RemoteController, OVSKernelSwitch, UserAP
+from mininet.node import RemoteController, OVSKernelSwitch
 
 
 def topology(pathmanager, scheduler):
@@ -22,7 +22,7 @@ def topology(pathmanager, scheduler):
     #  FOLDER_NAME = "n-n-ch1-ch11"
     "Create a network."
     net = Mininet(controller=RemoteController,
-                  link=TCLink, switch=OVSKernelSwitch, accessPoint=UserAP)
+                  link=TCLink, switch=OVSKernelSwitch)
     net.start()
     print "*** Creating nodes"
     sta1 = net.addStation(
@@ -30,14 +30,14 @@ def topology(pathmanager, scheduler):
     # ap2 = net.addAccessPoint('ap2', mac='00:00:00:00:00:02', equipmentModel='TLWR740N',
     #                          protocols='OpenFlow10', ssid='ssid_ap2', mode='n', channel='1', position='55,20,0')
     # ap3 = net.addAccessPoint('ap3', mac='00:00:00:00:00:03', equipmentModel='TLWR740N',
-    #                          protocols='OpenFlow10', ssid='ssid_ap3', mode='n', channel='11', position='50,11,0')
-    ap2 = net.addAccessPoint('ap2', mac='00:00:00:00:00:02', protocols='OpenFlow10', ssid='ssid_ap2', mode='n', channel='1', position='55,20,0')
-    ap3 = net.addAccessPoint('ap3', mac='00:00:00:00:00:03', protocols='OpenFlow10', ssid='ssid_ap3', mode='n', channel='11', position='50,11,0')
+    #                          protocols='OpenFlow10', ssid='ssid_ap3', mode='n', channel='6', position='50,11,0')
+    ap2 = net.addAccessPoint('ap2', mac='00:00:00:00:00:20', protocols='OpenFlow10', ssid='ssid_ap2', mode='n', channel='1', position='55,20,0')
+    ap3 = net.addAccessPoint('ap3', mac='00:00:00:00:00:30', protocols='OpenFlow10', ssid='ssid_ap3', mode='n', channel='11', position='50,11,0')
     h4 = net.addHost('h4', mac='00:00:00:00:00:04', ip='10.0.0.254/8')
     h5 = net.addHost('h5', mac='00:00:00:00:00:05', ip='192.168.0.254/24')
-    s6 = net.addSwitch('s6', mac='00:00:00:00:00:06', protocols='OpenFlow10')
-    s7 = net.addSwitch('s7', mac='00:00:00:00:00:07', protocols='OpenFlow10')
-    s8 = net.addSwitch('s8', mac='00:00:00:00:00:08', protocols='OpenFlow10')
+    # s6 = net.addSwitch('s6', mac='00:00:00:00:00:06', protocols='OpenFlow10')
+    # s7 = net.addSwitch('s7', mac='00:00:00:00:00:07', protocols='OpenFlow10')
+    # s8 = net.addSwitch('s8', mac='00:00:00:00:00:08', protocols='OpenFlow10')
     s9 = net.addSwitch('s9', mac='00:00:00:00:00:09', protocols='OpenFlow10')
     h10 = net.addHost('h10', mac='00:00:00:00:00:10', ip='192.168.1.254/24')
     c11 = net.addController('c11', controller=RemoteController, ip='127.0.0.1')
@@ -50,12 +50,12 @@ def topology(pathmanager, scheduler):
     net.addLink(ap3, sta1)
     net.addLink(ap2, h4, bw=100)
     net.addLink(ap3, h5, bw=100)  #
-    net.addLink(s6, h4, bw=1000)
-    net.addLink(s6, h5, bw=1000)  #
-    net.addLink(s6, s7, bw=1000)  #
-    net.addLink(s6, s8, bw=1000)
-    net.addLink(s7, s9, bw=1000)  #
-    net.addLink(s8, s9, bw=1000)
+    # net.addLink(s6, h4, bw=1000)
+    # net.addLink(s6, h5, bw=1000)  #
+    # net.addLink(s6, s7, bw=1000)  #
+    # net.addLink(s6, s8, bw=1000)
+    net.addLink(h4, s9, bw=1000)  #
+    net.addLink(h5, s9, bw=1000)
     net.addLink(s9, h10, bw=1000)
 
     h4.cmd('ifconfig h4-eth1 192.168.1.1/24')
@@ -81,9 +81,9 @@ def topology(pathmanager, scheduler):
     print "*** Starting network"
     net.build()
     c11.start()
-    s6.start([c11])
-    s7.start([c11])
-    s8.start([c11])
+    # s6.start([c11])
+    # s7.start([c11])
+    # s8.start([c11])
     s9.start([c11])
     ap2.start([c11])
     ap3.start([c11])
