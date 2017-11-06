621e577b5c72436de66102d0221f0b752b63d06d
Grzesiek <fundaysunday6@gmail.com>
2017-11-06 17:47:15 +0100

    Add some plots

--- test2nodes/n-n-ch1-ch6-iter3/results/calcMeanAndStdDev.py
+++ test2nodes/n-n-ch1-ch6-iter3/results/calcMeanAndStdDev.py
@@ -1,11 +1,24 @@
+#!/usr/bin/python
+
+"""
+calculating stats
+created by:
+Grzegorz Przybylo
+University of Science and Technology in Cracow
+Faculty of Computer Science, Electronics and Telecomunications
+ICT
+"""
+
 import os
+import re
 import sys
 import statistics
 
 
 def main(argv):
+    filenameToSave="statsResultsTcp.txt"
     try:
-        os.remove("statsResultsTcp.txt")
+        os.remove(filenameToSave)
     except OSError:
         pass
     for file in argv[1:]:
@@ -13,14 +26,15 @@ def main(argv):
         with open(file, 'r') as f:
             for line in f:
                 line = line.split(" ")
-                results.append(float(line[1]))
+		if re.match(r'\d+[.]{0,1}\d+', line[1]):
+                	results.append(float(line[1]))
         meanVal = statistics.mean(results)
         stdDevVal = statistics.stdev(results)
         pm = file.split("_")[1]
         sched = file.split("_")[2].split(".")[0]
         meanString = "Mean value for is: " + str(meanVal)
         stdDevString = "Standard Deviation value for is: " + str(stdDevVal)
-        with open("statsResultsTcp.txt", "a+") as fw:
+        with open(filenameToSave, "a+") as fw:
             fw.write("Path Manager: " + pm + " and Scheduler: " + sched + "\n")
             fw.write(meanString + "\n")
             fw.write(stdDevString + "\n")
--- test2nodes/n-n-ch1-ch6-iter4/results/continous/gnuplotter.sh
+++ test2nodes/n-n-ch1-ch6-iter4/results/continous/gnuplotter.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
-FILES=../client2/client_*
+
+<<'COMMENT'
+MPTCP performance test
+created by:
+Grzegorz Przybylo
+University of Science and Technology in Cracow
+Faculty of Computer Science, Electronics and Telecomunications
+ICT
+COMMENT
+
+CLIENT_FOLDER=client
+FILES=../$CLIENT_FOLDER/client_*
 for FILE in $FILES; do
+	sed -i '/mptcp/d' $FILE
 	filename=$(basename $FILE)
 	gnuplot <<- EOF
 		set terminal png
 		set tics font "Helvetica,10"
 		set ylabel "Mbit/s"
 		set xlabel "s"
-		set output "plots/client2/${filename}.png"
+		set output "plots/${CLIENT_FOLDER}/${filename}.png"
 		plot "${FILE}" title "${FILE}" with linespoints
 EOF
 done
