d69c0195e500af8966761b2d764211aebe9099e6
floppydrive6 <grzeg.przybylo@gmail.com>
2017-11-12 13:48:09 +0100

    Update client-get_results.sh

--- refactored/testmobility/refactored/results/client-get_results.sh
+++ refactored/testmobility/refactored/results/client-get_results.sh
@@ -4,7 +4,7 @@
 MPTCP performance test
 created by:
 Grzegorz Przybylo
-University of Science and Technology in Cracow
+AGH, University of Science and Technology in Cracow
 Faculty of Computer Science, Electronics and Telecomunications
 ICT
 COMMENT
@@ -14,6 +14,4 @@ for f in $FILES
 do
 	filename=$(basename $f)
 	cat $f | grep sec | head -$(cat $f | wc -l) | tr - " " | awk '{print $3,$8}' | head -n -1 > $filename
-	#bash continous_time.sh $filename
-	#echo "This is file $f"
 done
