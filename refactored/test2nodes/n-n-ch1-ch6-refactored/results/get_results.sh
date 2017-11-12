492c9c689cc654d3e03e6f639a06ccd7c8e15afb
floppydrive6 <grzeg.przybylo@gmail.com>
2017-11-12 13:38:52 +0100

    Update get_results.sh

--- refactored/test2nodes/n-n-ch1-ch6-refactored/results/get_results.sh
+++ refactored/test2nodes/n-n-ch1-ch6-refactored/results/get_results.sh
@@ -4,6 +4,4 @@ for f in $FILES
 do
 	filename=$(basename $f)
 	cat $f | grep sec | head -60 | tr - " " | awk '{print $3,$8}' > $filename
-	#bash continous_time.sh $filename
-	#echo "This is file $f"
 done
