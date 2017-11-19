c505a036eceddded6012f191affd3260f20d9da8
floppydrive6 <grzeg.przybylo@gmail.com>
2017-11-19 22:09:08 +0100

    Update manual-test-technologies.py

--- refactored/testtechnologies/manual-test-technologies.py
+++ refactored/testtechnologies/manual-test-technologies.py
@@ -61,6 +61,18 @@ def topology(pathmanager, scheduler):
     net.addLink(s8, s9, bw=1000)
     net.addLink(s9, h10, bw=1000)
 
+    print"*** Plot graph ***"
+    net.plotGraph(max_x=100, max_y=100)
+
+    print "*** Starting network"
+    net.build()
+    c11.start()
+    s6.start([c11])
+    s7.start([c11])
+    s8.start([c11])
+    s9.start([c11])
+    ap2.start([c11])
+    
     h4.cmd('ifconfig h4-eth1 192.168.1.1/24')
     h5.cmd('ifconfig h5-eth1 192.168.1.2/24')
 
@@ -96,15 +108,6 @@ def topology(pathmanager, scheduler):
     #sta1.cmd('iw dev sta1-wlan0 disconnect')
     #sta1.cmd('ifconfig sta1-eth1 down')
 
-    print "*** Starting network"
-    net.build()
-    c11.start()
-    s6.start([c11])
-    s7.start([c11])
-    s8.start([c11])
-    s9.start([c11])
-    ap2.start([c11])
-
     h10.cmd('ip route add 10.0.0.0/8 via 192.168.1.1')
     h10.cmd('ip route add 192.168.0.0/24 via 192.168.1.2')
     h10.cmd('ip route add 192.168.2.0/24 via 192.168.1.3')
@@ -122,9 +125,6 @@ def topology(pathmanager, scheduler):
     sleep(2)
 
     print "starting simulation for path_manager: ", pathmanager, " and scheduler: ", scheduler
-
-    print"*** Plot graph ***"
-    net.plotGraph(max_x=100, max_y=100)
 
     print "*** Running CLI"
     CLI(net)
